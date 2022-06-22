bb613a366a77c33bfaa043ea7d8aac0b7b944065
ThieryNisengwe <nisengwethiery@gmail.com>
2022-06-22 09:28:57 -0700

    js

---
+++ algos/w1d2Algo.js
@@ -0,0 +1,19 @@
+/*
+Data types
+*/
+var name = "Thiery Nis";
+var age = "uknown";
+var chipotlePrice = 9.75;
+var chipotle_price = 9.75;
+var isTeachingToday = false;
+// console.log("Wazzaaaa " + name);
+
+
+
+var artist = ["Drake", "Weezy", "Weekend", "Taylor Swift", ]
+
+// console.log(artist)
+
+for( var i= 0; i<artist.length; i++) {
+  console.log(artist[i])
+}
