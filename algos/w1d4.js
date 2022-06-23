af822eae817ba4741207f7e8827e0e9babf8ec8f
ThieryNisengwe <nisengwethiery@gmail.com>
2022-06-23 14:35:06 -0700

    added things

---
+++ algos/w1d4.js
@@ -0,0 +1,23 @@
+// for(var i=0; i<10; i++) {
+//   console.log(i);
+//   i = i + 3;
+// }
+
+// console.log("outside of the loop " + i)
+
+// var artist = ["Drake", "J Cole", "MJ", "Taylor Swift","Beyonce"]
+
+// for(let i = 0; i<artist.length; i++) {
+//   // console.log(artist[0])
+//   if(artist[i] == "MJ") {
+//     console.log("King of Pop!");
+//   }else{
+//     console.log(artist[i]);
+//   }
+  // if the artist is MJ, then print "King of Pop!"
+  // for everybody else, just print their name
+// }
+
+for (let i = 10; i>=1; i--) {
+  console.log(i);
+}
