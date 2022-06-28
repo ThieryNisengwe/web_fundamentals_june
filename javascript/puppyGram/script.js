4b117a7ab963386e81da7a35581f5bb9a59feede
ThieryNisengwe <nisengwethiery@gmail.com>
2022-06-28 11:05:32 -0700

    uploaded

--- javascript/puppyGram/script.js
+++ javascript/puppyGram/script.js
@@ -39,3 +39,13 @@ function revertImg(element){
   console.log("hello", element);
   element.src = "./resources/puppy.jpeg"
 }
+
+function likeadd(id){
+  console.log("liking",id);
+  var element = document.querySelector(id);
+  console.log("element looks like this", element.innerText);
+  let likeCount = Number(element.innerText);
+  console.log(likeCount)
+  likeCount+=1;
+  element.innerText = likeCount;
+}
